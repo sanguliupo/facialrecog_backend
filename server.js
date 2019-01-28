526b33fac3df5101eb39524686e0b7cf825a3536
Vanessa Wang <sanguliupo@gmail.com>
2019-01-28 15:27:30 -0800

    connected backend to database

--- server.js
+++ server.js
@@ -2,6 +2,20 @@ const express = require(`express`);
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require ('knex');
+
+const db=knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'sanguliupo',
+    password : '',
+    database : 'smart-brain'
+  }
+});
+
+
+
 const app = express();
 
 app.use(bodyParser.json());
@@ -45,47 +59,56 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
 	const { email, name, password } = req.body;
-	bcrypt.hash(password, null, null, function(err, hash) {
-		console.log(hash);
-	});
-	database.users.push({
-		id: '125',
-		name: name,
-		email: email,
-		entries: 0,
-		joined: new Date().toString()
-	});
-	res.json(database.users[database.users.length - 1]);
-});
+	const hash=bcrypt.hashSync(password);
+	db.transaction(trx =>{
+		trx.insert({
+			hash:hash,
+			email:email
+		})
+		.into('login')
+		.returning('email')
+		.then(loginEmail=>{
+			return trx('users')
+				.returning('*')
+				.insert({
+					email:loginEmail[0],
+					name:name,
+					joined: new Date()
+				})
+				.then(user=>{
+					res.json(user[0]);
+				})
+		})
+		.then(trx.commit)
+		.catch(trx.rollback)
+	})
+	.catch(err=>res.status(400).json('unable to register'))
+})	
 
 app.get('/profile/:id', (req, res) => {
 	const { id } = req.params;
-	let found = false;
-	database.users.forEach(user => {
-		if (user.id === id) {
-			found = true;
-			return res.json(user);
+	db.select('*').from('users').where({id})
+	.then(user=>{
+		if(user.length){
+			res.json(user[0]);
+		}else{
+			res.status(400).json('Not found')
 		}
-	});
-	if (!found) {
-		res.status(404).json('no such user');
-	}
+	})
+	.catch (err=>res.status(400).json('error getting user'))
 });
 
 app.put('/image', (req, res) => {
 	const { id } = req.body;
-	let found = false;
-	database.users.forEach(user => {
-		if (user.id === id) {
-			found = true;
-			user.entries++;
-			return res.json(user.entries);
-		}
-	});
-	if (!found) {
-		res.status(404).json('no such user');
-	}
-});
+	db('users').where('id', '=', id)
+	  .increment('entries', 1)
+	  .returning('entries')
+	  .then(entries=>{
+	  	res.json(entries[0]);
+	  })
+	  .catch(err=>res.status(400).json('unable to get enrtries'))
+  })
+
 
 // Load hash from your password DB.
 // bcrypt.compare("bacon", hash, function(err, res) {
