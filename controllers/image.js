a12ba68233d8abce065e738e01c8e84ed88e28af
Vanessa Wang <sanguliupo@gmail.com>
2019-01-31 13:56:28 -0800

    moved clafrify api key from frontend to backend server

---
+++ controllers/image.js
@@ -0,0 +1,28 @@
+const Clarifai=require('clarifai');
+
+const app = new Clarifai.App({ apiKey: '5df2a1cd6ab145b3ae5c8965a0878107' });
+
+const handleApiCall=(req, res)=>{
+ app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+      .then(data=>{
+      	res.json(data);
+      })
+      .catch(err=>res.status(400).json('unable to work with API'))
+}
+
+const handleImage=(req, res, db) => {
+	const { id } = req.body;
+	db('users').where('id', '=', id)
+	  .increment('entries', 1)
+	  .returning('entries')
+	  .then(entries=>{
+	  	res.json(entries[0]);
+	  })
+	  .catch(err=>res.status(400).json('unable to get enrtries'))
+  }
+
+  module.exports={
+	handleImage,
+	handleApiCall
+}
--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require ('knex');
-
-const register =require('./controllers/register');
 const signin =require('./controllers/signin');
+const register =require('./controllers/register');
+const profile =require('./controllers/profile');
+const image =require('./controllers/image');
+
 
 const db=knex({
   client: 'pg',
@@ -24,67 +26,14 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const database = {
-	users: [
-		{
-			id: '123',
-			name: 'John',
-			email: 'john@gmail.com',
-			password: 'cookies',
-			entries: 0,
-			joined: new Date()
-		},
-		{
-			id: '124',
-			name: 'Sally',
-			email: 'sally@gmail.com',
-			password: 'banana',
-			entries: 0,
-			joined: new Date()
-		}
-	]
-};
 
-app.get('/', (req, res) => {
-	res.send(database.users);
-});
-
+app.get('/', (req, res) => {res.send(database.users)});
 app.post('/signin', (req, res)=>{signin.handleSignin(req, res, db, bcrypt)});
-
 app.post('/register', (req, res)=>{register.handleRegister(req, res, db, bcrypt)});	
+app.get('/profile/:id', (req, res)=>{profile.handleProfileGet(req, res, db)});
+app.put('/image', (req, res)=>{image.handleImage(req, res, db)});
+app.post('/imageurl', (req, res)=>{image.handleApiCall(req, res)});
 
-app.get('/profile/:id', (req, res) => {
-	const { id } = req.params;
-	db.select('*').from('users').where({id})
-	.then(user=>{
-		if(user.length){
-			res.json(user[0]);
-		}else{
-			res.status(400).json('Not found')
-		}
-	})
-	.catch (err=>res.status(400).json('error getting user'))
-});
-
-app.put('/image', (req, res) => {
-	const { id } = req.body;
-	db('users').where('id', '=', id)
-	  .increment('entries', 1)
-	  .returning('entries')
-	  .then(entries=>{
-	  	res.json(entries[0]);
-	  })
-	  .catch(err=>res.status(400).json('unable to get enrtries'))
-  })
-
-
-// Load hash from your password DB.
-// bcrypt.compare("bacon", hash, function(err, res) {
-//     // res == true
-// });
-// bcrypt.compare("veggies", hash, function(err, res) {
-//     // res = false
-// });
 
 app.listen(3000, () => {
 	console.log(`app is running on port 3000`);
