52bf1cbc2bc15369d9f0b7b9da5fa0865a3a0aa0
Vanessa Wang <sanguliupo@gmail.com>
2019-01-31 20:51:56 -0800

    update port setting

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.put('/image', (req, res)=>{image.handleImage(req, res, db)});
 app.post('/imageurl', (req, res)=>{image.handleApiCall(req, res)});
 
 
-app.listen(3000 ||process.env.PORT, () => {
+app.listen(process.env.PORT||3000, () => {
 	console.log(`app is running on port ${process.env.PORT}`);
 });
 
