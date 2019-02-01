732285d763612dd354ce9d47b32c795943bed0a2
Vanessa Wang <sanguliupo@gmail.com>
2019-01-31 18:00:31 -0800

    modified server

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image =require('./controllers/image');
 const db=knex({
   client: 'pg',
   connection: {
-    host : '1postgresql-slippery-84148',
-    user : 'sanguliupo',
-    password : '',
-    database : 'smart-brain'
+    connectionString : process.env.DATABASE_URL,
+    ssl:true,
   }
 });
 
