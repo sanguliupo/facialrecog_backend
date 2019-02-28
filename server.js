1e98b8f029147678b38391ef4ff4ad1ae4e3a0b7
Vanessa Wang <sanguliupo@gmail.com>
2019-02-27 18:32:05 -0800

    Remove empty lines

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const register =require('./controllers/register');
 const profile =require('./controllers/profile');
 const image =require('./controllers/image');
 
-
 const db=knex({
   client: 'pg',
   connection: {
@@ -16,8 +15,6 @@ const db=knex({
     ssl:true,
   }
 });
-
-
 
 const app = express();
 
@@ -32,12 +29,9 @@ app.get('/profile/:id', (req, res)=>{profile.handleProfileGet(req, res, db)});
 app.put('/image', (req, res)=>{image.handleImage(req, res, db)});
 app.post('/imageurl', (req, res)=>{image.handleApiCall(req, res)});
 
-
 app.listen(process.env.PORT||3000, () => {
 	console.log(`app is running on port ${process.env.PORT}`);
 });
-
-
 
 /*
 /-->res=this is working
